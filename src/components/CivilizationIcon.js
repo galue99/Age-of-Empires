456c848f0bfb3405636ce18ff54f9bf2f3d972ac
Edgar Espinetti <eespinetti@arquant.com.ar>
2023-10-26 14:35:34 -0300

    Add styles

--- src/components/CivilizationIcon.js
+++ src/components/CivilizationIcon.js
@@ -2,14 +2,9 @@ import React from 'react';
 
 const CivilizationIcon = ({ imageUrl, altText }) => (
   <img
+    className="icon-detail"
     src={imageUrl}
     alt={altText}
-    style={{
-      width: '80px',
-      borderRadius: '50%',
-      marginBottom: '20px',
-      boxShadow: '1px 1px 6px rgba(0,0,0,0.2)'
-    }}
   />
 );
 
